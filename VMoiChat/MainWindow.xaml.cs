4a5c381b6ea5fd88f0003e376b0dc8cd55bd49b1
endliar <endliar@yandex.ru>
2024-01-07 21:53:19 +0700

    Добавьте файлы проекта.

---
+++ VMoiChat/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+
+namespace VMoiChat
+{
+    public partial class MainWindow : Window
+    {
+        private TcpServer tcpServer;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddMessageToListBox(string message)
+        {
+            MessageListBox.Items.Add(message);
+        }
+
+        private void Window_Closing(object render, System.ComponentModel.CancelEventArgs e)
+        {
+            tcpServer.Stop();
+        }
+
+        private void UpdateServerStatus(bool running)
+        {
+            ServerStatusTextBlock.Text = "Server status: " + (running ? "Running" : "Stopped");
+        }
+
+        private async void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputTextBox.Text) && !string.IsNullOrEmpty(InputNameRecipientBox.Text) && tcpServer != null)
+            {
+                string username = InputNameRecipientBox.Text;
+                string message = InputTextBox.Text;
+
+                Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox($"{username}, {message}"); });
+                InputTextBox.Clear();
+
+                await tcpServer.SendMessageToClient(username, message);
+            }
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            tcpServer = new TcpServer(8888);
+            tcpServer.Start();
+            UpdateServerStatus(true);
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (tcpServer != null)
+            {
+                tcpServer.Stop();
+                tcpServer = null;
+                UpdateServerStatus(false);
+            } else
+            {
+                Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox("Сервер уже остановлен, узбагойся"); });
+            }
+        }
+
+        private async void ConnectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            IPAddress ipAddress;
+            int port;
+
+            if (!string.IsNullOrEmpty(InputNameBox.Text) && tcpServer != null)
+            {
+                ipAddress = IPAddress.Parse("127.0.0.1");
+                port = 8888;
+                string username = InputNameBox.Text;
+                await tcpServer.StartClient(ipAddress, port, username);
+            }
+
+        }
+
+        private async void DisconnectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(InputNameBox.Text) && tcpServer != null)
+            {
+                string username = InputNameBox.Text;
+                await tcpServer.DisconnectClient(username);
+            }
+        }
+    }
+}
---
+++ VMoiChat/TcpServer.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace VMoiChat
+{
+    public class TcpServer
+    {
+        private TcpListener _tcpListener;
+        private CancellationTokenSource cancellationTokenSource;
+
+        private List<ConnectedClient> connectedClients = new List<ConnectedClient>();
+
+        public TcpServer(int port) 
+        {
+            _tcpListener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public async void Start()
+        {
+            try
+            {
+                _tcpListener.Start();
+                cancellationTokenSource = new CancellationTokenSource();
+                string receivedText = "Сервер успешно запущен";
+                Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox(receivedText); });
+                ListenForClients(_tcpListener, cancellationTokenSource.Token);
+
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}", "Server Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private async void ListenForClients(TcpListener listener, CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    TcpClient tcpClient = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+                    HandleClientAsync(tcpClient, token);
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("SocketException обработано: " + ex.Message);
+            }
+        }
+
+        public async void HandleClientAsync(TcpClient tcpClient, CancellationToken token)
+        {
+            try
+            {
+                byte[] buffer = new byte[4096];
+                NetworkStream networkStream = tcpClient.GetStream();
+
+                while (!token.IsCancellationRequested)
+                {
+                    int bytesRead = await networkStream.ReadAsync(buffer, 0, buffer.Length, token).ConfigureAwait(false);
+                    if (bytesRead == 0) break;
+                    await networkStream.WriteAsync(buffer, 0, bytesRead, token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Console.WriteLine("Операция была отменена.");
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            } 
+            finally 
+            {
+                tcpClient.Close();
+            }
+        }
+
+        public async Task StartClient(IPAddress serverIpAddress, int port, string message)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            await tcpClient.ConnectAsync(serverIpAddress, port).ConfigureAwait(false);
+
+            NetworkStream? networkStream = null;
+            StreamReader? streamReader = null;
+
+            try
+            {
+                networkStream = tcpClient.GetStream();
+                streamReader = new StreamReader(networkStream, Encoding.UTF8);
+
+                byte[] data = Encoding.UTF8.GetBytes(message);
+
+                await networkStream.WriteAsync(data, 0, data.Length).ConfigureAwait(false);
+                char[] buffer = new char[4096];
+
+                int bytesRead = await streamReader.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+                string receivedMessage = new string(buffer, 0, bytesRead);
+
+                ConnectedClient connectedClient = new ConnectedClient { TcpClient = tcpClient, Username = receivedMessage };
+
+                connectedClients.Add(connectedClient);
+                Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox(receivedMessage + " Подключился к серверу"); });
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public async Task SendMessageToClient(string username, string message)
+        {
+            ConnectedClient connectedClient = connectedClients.FirstOrDefault(client => client.Username == username);
+
+            if (connectedClient != null)
+            {
+                NetworkStream networkStream = connectedClient.TcpClient.GetStream();
+                StreamWriter streamWriter = new StreamWriter(networkStream, Encoding.UTF8);
+
+                try
+                {
+                    await streamWriter.WriteLineAsync(message).ConfigureAwait(false);
+                    await streamWriter.FlushAsync().ConfigureAwait(false);
+                    Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox($"Ответ от {connectedClient.Username}: {message}"); });
+
+                } catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        public async Task DisconnectClient(string username)
+        {
+            ConnectedClient connectedClient = connectedClients.FirstOrDefault(client => client.Username == username);
+
+            if (connectedClient != null)
+            {
+                if (connectedClient.TcpClient.Connected)
+                {
+                    NetworkStream networkStream = connectedClient.TcpClient.GetStream();
+                    if (networkStream != null)
+                    {
+                        networkStream.Close();
+                    }
+                    connectedClient.TcpClient.Close();
+                }
+                connectedClients.Remove(connectedClient);
+
+                Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox($"{username} отключился от сервера"); });
+            }
+        }
+
+
+        public void Stop() 
+        {
+            _tcpListener.Stop();
+            cancellationTokenSource.Cancel();
+
+            foreach (var client in connectedClients)
+            {
+                if (client.TcpClient.Connected)
+                {
+                    try
+                    {
+                        NetworkStream networkStream = client.TcpClient.GetStream();
+                        if (networkStream != null)
+                        {
+                            networkStream.Close();
+                        }
+                        client.TcpClient.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine($"Error disconnecting client {client.Username}: {ex.Message}");
+                    }
+                }
+            }
+
+            connectedClients.Clear();
+            Application.Current.Dispatcher.Invoke(() => { ((MainWindow)Application.Current.MainWindow).AddMessageToListBox("Сервер остановлен"); });
+        }
+    }
+}
